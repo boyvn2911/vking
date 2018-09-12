a082b59de7537ab13373457f764ab7f5a2043da2
Tuấn Sơn <37967607+boyvn2911@users.noreply.github.com>
2018-09-12 23:30:14 +0700

    master layout 1

---
+++ resources/views/header.blade.php
@@ -0,0 +1,30 @@
+<div id="header">
+    <section id="header-top">
+        <div class="container">
+            <div class="row justify-content-center">
+                free shipping on orders over $250
+            </div>
+        </div>
+    </section>
+
+    <section id="header-bot">
+        <div class="container">
+            <div class="row justify-content-between">
+                <div id="social">
+                    <a href="" class="social-icon"><i class="fab fa-instagram"></i></a>
+                    <a href="" class="social-icon"><i class="fab fa-facebook-f"></i></a>
+                </div>
+                <div id="header-nav">
+                    <ul>
+                        <li>thương hiệu</li>
+                        <li><a href=""><img src="resources/assets/image/logo.png"></a></li>
+                        <li>phân loại</li>
+                    </ul>
+                </div>
+                <div id="search">
+                    <a><i class="fas fa-search"></i></a>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
