6e240c6ad84b01cb7af2fad981e167d033a414c3
boyvn2911 <boyvn2911@gmail.com>
2018-10-25 17:54:14 +0700

    a

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -1,1 +1,155 @@
 @extends('guest.layout')
+
+@section('css')
+    <style>
+
+        #item-hot {
+            padding: 80px 0 180px;
+        }
+
+        .item {
+            position: relative;
+            cursor: pointer;
+        }
+
+        .item > .image {
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: contain;
+            margin-bottom: 25px;
+        }
+
+        .item > .image:after {
+            content: "";
+            display: block;
+            padding-top: 100%;
+        }
+
+        .item > .detail {
+            display: none;
+            position: absolute;
+            left: 0;
+            bottom: 0;
+            right: 0;
+            background-color: rgba(0, 0, 0, 0.70);
+            color: #FFF;
+            padding: 15px;
+        }
+
+        .item:hover > .detail {
+            display: block;
+        }
+
+        .item > .detail > .name {
+            text-transform: uppercase;
+        }
+
+        #special-offer {
+            background-image: url();
+        }
+    </style>
+@stop
+
+@section('script')
+@stop
+
+@section('main')
+    <section id="banner">
+
+    </section>
+
+    <section id="item-hot">
+        <div class="container">
+            <div class="text-center">
+                <h2>
+                    ITEM HOT
+                    <span class="line-decor"><i class="far fa-square"></i></span>
+                </h2>
+
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="item">
+                        <div class="image"
+                             style="background-image: url(https://cdn.raffaello-network.com/ti%E1%BA%BFng-vi%E1%BB%87t/fashion-details/461877/1541/gucci-qu%E1%BA%A7n-%C3%81o-tr%E1%BA%BB-em_gukclo-504253x9p07x9p07-4212-medium-1.jpg);"></div>
+                        <div class="detail">
+                            <div class="name">Áo Phông đen</div>
+                            <div class="brand">Gucci</div>
+                            <div class="price">
+                                <span class="price-org">999.000</span>
+                                <span class="price-vking">800.000</span>
+                                <span class="price-sale">400.000</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="item">
+                        <div class="image"
+                             style="background-image: url(https://cdn.raffaello-network.com/ti%E1%BA%BFng-vi%E1%BB%87t/fashion-details/461877/1541/gucci-qu%E1%BA%A7n-%C3%81o-tr%E1%BA%BB-em_gukclo-504253x9p07x9p07-4212-medium-1.jpg);"></div>
+                        <div class="detail">
+                            <div class="name">Áo Phông đen</div>
+                            <div class="brand">Gucci</div>
+                            <div class="price">
+                                <span class="price-org">999.000</span>
+                                <span class="price-vking">800.000</span>
+                                <span class="price-sale">400.000</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="item">
+                        <div class="image"
+                             style="background-image: url(https://cdn.raffaello-network.com/ti%E1%BA%BFng-vi%E1%BB%87t/fashion-details/461877/1541/gucci-qu%E1%BA%A7n-%C3%81o-tr%E1%BA%BB-em_gukclo-504253x9p07x9p07-4212-medium-1.jpg);"></div>
+                        <div class="detail">
+                            <div class="name">Áo Phông đen</div>
+                            <div class="brand">Gucci</div>
+                            <div class="price">
+                                <span class="price-org">999.000</span>
+                                <span class="price-vking">800.000</span>
+                                <span class="price-sale">400.000</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="item">
+                        <div class="image"
+                             style="background-image: url(https://cdn.raffaello-network.com/ti%E1%BA%BFng-vi%E1%BB%87t/fashion-details/461877/1541/gucci-qu%E1%BA%A7n-%C3%81o-tr%E1%BA%BB-em_gukclo-504253x9p07x9p07-4212-medium-1.jpg);"></div>
+                        <div class="detail">
+                            <div class="name">Áo Phông đen</div>
+                            <div class="brand">Gucci</div>
+                            <div class="price">
+                                <span class="price-org">999.000</span>
+                                <span class="price-vking">800.000</span>
+                                <span class="price-sale">400.000</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="special-offer">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="spe-container">
+                        <h2>SPECIAL OFFER
+                            <span class="line-decor"><i class="far fa-square"></i></span>
+                        </h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has
+                            been
+                            the industry's standard dummy text ever since the 1500s, </p>
+
+                        <a>XEM TIẾP <i class="fas fa-arrow-right"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="new-arrival"></section>
+@stop
--- resources/views/guest/layout.blade.php
+++ resources/views/guest/layout.blade.php
@@ -6,18 +6,36 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
     <title>VKing Authentic | Dashboard</title>
-
+    <link rel="stylesheet" href="resources/assets/css/reset.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css"
+          integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+    <link rel="stylesheet" href="resources/assets/css/base.css">
+    <link rel="stylesheet" href="resources/assets/css/header.css">
+    <link rel="stylesheet" href="resources/assets/css/footer.css">
     @yield('css')
 </head>
 <body>
 <div id="wrapper">
     @include('guest.partials.header')
 
-    @yield('main')
+    <div id="main">
+        @yield('main')
+    </div>
 
     @include('guest.partials.footer')
 </div>
 
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
 @yield('script')
 </body>
 </html>
